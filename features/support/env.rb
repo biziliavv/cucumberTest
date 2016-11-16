8d1ef6b72d36569c33ee0b5d00a7803171c01249
vitaliybizilia <biziliavv@gmail.com>
2016-11-16 14:40:09 +0200

    Edited price lists

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,7 @@
 case ENV['BROWSER']
   when 'chrome'
-    Selenium::WebDriver::Chrome::Service.executable_path = '/usr/local/bin/chromedriver'
-    @browser = Watir::Browser.new :chrome
+    @browser = Selenium::WebDriver::Chrome::Service.executable_path = '/Users/vitaliybizilia/Documents/cucumberTest/chromedriver'
+
   when 'firefox'
-    @browser = Watir::Browser.new :firefox
+    @browser = Selenium::WebDriver::Firefox::Service.executable_path = '/Users/vitaliybizilia/Documents/cucumberTest/geckodriver'
 end
