d532b810e7a215f2bcb77afd3c5c8e7fe336d30b
vitaliybizilia <biziliavv@gmail.com>
2016-11-16 14:00:35 +0200

    Edited price lists

--- features/step_definitions/price_lists.rb
+++ features/step_definitions/price_lists.rb
@@ -1,8 +1,7 @@
 
 
 Given(/^user opens pricelists tab$/) do
-  chromedriver_path = File.join(File.absolute_path('/usr/local/bin/', File.dirname(__FILE__)),"chromedriver")
-  Selenium::WebDriver::Chrome.driver_path = chromedriver_path
+
   @browser = Watir::Browser.new :chrome
   @browser.goto "http://testdemo.easyerp.com/#easyErp/priceLists/list"
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,1 +1,6 @@
-Selenium::WebDriver::Chrome::Service.executable_path = '/usr/local/bin/chromedriver'
+case ENV['BROWSER']
+  when 'chrome'
+    browser = Watir::Browser.new :chrome
+  when 'firefox'
+    browser = Watir::Browser.new :firefox
+end
