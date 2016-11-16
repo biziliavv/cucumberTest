50140ce762ac4844e51e67b29ebf6f56dc441cf1
vitaliybizilia <biziliavv@gmail.com>
2016-11-16 15:52:09 +0200

    Edited price lists

--- features/step_definitions/price_lists.rb
+++ features/step_definitions/price_lists.rb
@@ -1,7 +1,9 @@
 require 'chromedriver/helper'
+require 'headless'
 
 Given(/^user opens pricelists tab$/) do
-
+  headless = Headless.new
+  headless.start
   @browser = Watir::Browser.new :chrome
   @browser.goto "http://testdemo.easyerp.com/#easyErp/priceLists/list"
 end
@@ -82,6 +84,7 @@ end
 Then(/^browser is closing$/) do
   sleep(2)
   @browser.close
+  headless.destroy
 end
 
 When(/^user checks one item$/) do
