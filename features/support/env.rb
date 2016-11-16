d1c3769d25e7da439e2b8caa7866e2197a817a32
vitaliybizilia <biziliavv@gmail.com>
2016-11-16 14:21:22 +0200

    Edited price lists

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,7 @@
 case ENV['BROWSER']
   when 'chrome'
     Selenium::WebDriver::Chrome::Service.executable_path = '/usr/local/bin/chromedriver'
-    browser = Watir::Browser.new :chrome
+    @browser = Watir::Browser.new :chrome
   when 'firefox'
-    browser = Watir::Browser.new :firefox
+    @browser = Watir::Browser.new :firefox
 end
